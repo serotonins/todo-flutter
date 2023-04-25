bf5660c88a9d64aeb26d50d1b59a620f924345d0
serot <serotonins023@gmail.com>
2023-04-25 22:50:24 +0900

    시작일 마감일 설정 가능

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+// import 'screen/addTodoScreen.dart';
 
+// 필요 1
 void main() {
   runApp(const MyApp());
 }
 
+// 필요 2
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -22,6 +27,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 필요 3
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -34,8 +40,10 @@ class MyHomePage extends StatefulWidget {
 class TodoType {
   bool isDone = false; // 완료했는지
   String title; // 할일 이름
+  DateTime? startDate = DateTime.now();
+  DateTime? endDate = DateTime.now();
 
-  TodoType(this.title);
+  TodoType(this.title, this.startDate, this.endDate);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -43,8 +51,10 @@ class _MyHomePageState extends State<MyHomePage> {
   var _cTodo = '시작일 순';
   final _todoListItem = <TodoType>[];
   var _todoController = TextEditingController(); // 입력받은 투두 문자열을 조작하는 컨트롤러래
+  var _dateTimeEditingController = TextEditingController(); // 날짜 받아오는 컨트롤러도 하나 만들어줌
+  DateTime _selectedDate = DateTime.now();
 
-  void _addTodo(TodoType todo) {
+  void _addTodo(TodoType todo) { // 리스트에 투두 아이템 하나 추가
     setState(() {
       _todoListItem.add(todo);
       _todoController.text = '';
@@ -65,14 +75,98 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _changetodo(bool ma) {
     setState(() {
-      if (ma == true) {_cTodo = '마감일 순';}
-      else {_cTodo = '시작일 순';};
+      if (ma == true) {_cTodo = '마감일 기준';}
+      else {_cTodo = '시작일 기준';};
     });
+  }
+
+  // 날짜, 시간 연달아 선택해서 _selectedDate에 넣어주는 함수
+  Future jdatePicker(jdate) async {
+    _selectedDate = jdate;
+
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: jdate,
+        firstDate: jdate,
+        lastDate: DateTime(2999),
+    );
+    if (picked != null) { setState(() {
+      _selectedDate = picked;
+    });}
+
+    TimeOfDay? tpicked = await showTimePicker(context: context, initialTime: TimeOfDay.now());
+    if (tpicked != null && _selectedDate != null) {
+      setState(() {
+        _selectedDate = _selectedDate.add(Duration(hours: tpicked.hour, minutes: tpicked.minute));
+      });
+    }
+  }
+
+  // todo 추가하는 alert dialog
+  Future<dynamic> showStatefulDialogBuilder(BuildContext context) async {
+    DateTime _startDate = DateTime.now();
+    DateTime _endDate = DateTime.now();
+
+    await showDialog<void>(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text("할 일이 또 있나요?", style: TextStyle(fontSize: 15),),
+            content: StatefulBuilder(
+                builder: (__, StateSetter setState) {
+                  return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          controller: _todoController,
+                        ),
+                        Row(
+                          children: [
+                            TextButton(
+                              onPressed: () async {
+                                await jdatePicker(DateTime.now());
+                                setState(() {
+                                  _startDate = _selectedDate;
+                                });
+                                print(_startDate);
+                              },
+                              child: Text(DateFormat('yyyy/MM/dd HH:mm').format(_startDate)),
+                            ),
+                            Text(" 부터  "),
+                            TextButton(
+                                onPressed: () async {
+                                  await jdatePicker(_startDate);
+                                  setState(() {
+                                    _endDate = _selectedDate;
+                                  });
+                                },
+                                child: Text(DateFormat('yyyy/MM/dd HH:mm').format(_endDate)),
+                            ),
+                            Text(" 까지"),
+                          ],
+                        )
+                      ]
+                  );
+                }
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    _addTodo(TodoType(_todoController.text, _startDate, _endDate));
+                    Navigator.pop(context);
+                  },
+                  child: Text('추가', style: TextStyle(color: Colors.indigo),),
+              )
+            ]
+          );
+        }
+    );
   }
 
   @override
   void dispose() {
-    _todoController.dispose(); // 컨트롤러 종료시 해제해주는 코드래
+    _todoController.dispose(); // widget이 dispose될 때 컨트롤러도 같이 해줘라
+    _dateTimeEditingController.dispose();
     super.dispose();
   }
 
@@ -84,11 +178,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: <Widget>[],
       ),
       body: Center(
-
         child: Column(
-
           // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
@@ -118,77 +211,66 @@ class _MyHomePageState extends State<MyHomePage> {
                   _cTodo,
                 )
               ]
-            )
+            ),
+            // Text(DateFormat('yyyy/MM/dd \nHH:mm').format(DateTime.now())) // 관찰용
           ],
         ),
       ),
-      floatingActionButton: multipleFloatingButton(), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {showStatefulDialogBuilder(context);},
+        tooltip: '추가',
+        child: const Icon(Icons.add),
+      )
+      // multipleFloatingButton(), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  Widget? multipleFloatingButton() {
-    return SpeedDial(
-      animatedIcon: AnimatedIcons.menu_close,
-      visible: true,
-      curve: Curves.bounceIn,
-      // shape: CircleBorder(),
-      backgroundColor: Colors.indigo[100],
-      children: [
-        SpeedDialChild(
-          child: const Icon(Icons.settings_sharp, color: Colors.indigo,),
-          shape: CircleBorder(),
-          backgroundColor: Colors.indigo[100],
-          label: "설정",
-          labelBackgroundColor: Colors.indigo[50],
-          labelStyle: const TextStyle(
-            fontWeight: FontWeight.w500,
-            color: Colors.black54,
-            fontSize: 13.0
-          ),
-          onTap: () {}
-        ),
-        SpeedDialChild(
-          child: const Icon(Icons.add, color: Colors.indigo,),
-          shape: CircleBorder(),
-          backgroundColor: Colors.indigo[100],
-          label: "할 일 추가",
-          labelBackgroundColor: Colors.indigo[50],
-          labelStyle: const TextStyle(
-            fontWeight: FontWeight.w500,
-            color: Colors.black54,
-            fontSize: 13.0
-          ),
-          onTap: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text("할 일이... 또 생겼니..? 힘내라...", style: TextStyle(fontSize: 15),),
-                    content: TextField(
-                      controller: _todoController,
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          _addTodo(TodoType(_todoController.text));
-                          Navigator.pop(context);
-                        },
-                        child: Text('추가', style: TextStyle(color: Colors.indigo),),
-                      )
-                    ],
-                  );
-                }
-            );
-          },
-        )
-      ],
-    );
-  }
+  // multiplefloatingbutton
+  // Widget? multipleFloatingButton() {
+  //   return SpeedDial(
+  //     animatedIcon: AnimatedIcons.menu_close,
+  //     visible: true,
+  //     curve: Curves.bounceIn,
+  //     // shape: CircleBorder(),
+  //     backgroundColor: Colors.indigo[100],
+  //     children: [
+  //       SpeedDialChild(
+  //         child: const Icon(Icons.settings_sharp, color: Colors.indigo,),
+  //         shape: CircleBorder(),
+  //         backgroundColor: Colors.indigo[100],
+  //         label: "설정",
+  //         labelBackgroundColor: Colors.indigo[50],
+  //         labelStyle: const TextStyle(
+  //           fontWeight: FontWeight.w500,
+  //           color: Colors.black54,
+  //           fontSize: 13.0
+  //         ),
+  //         onTap: () {}
+  //       ),
+  //       SpeedDialChild(
+  //         child: const Icon(Icons.add, color: Colors.indigo,),
+  //         shape: CircleBorder(),
+  //         backgroundColor: Colors.indigo[100],
+  //         label: "할 일 추가",
+  //         labelBackgroundColor: Colors.indigo[50],
+  //         labelStyle: const TextStyle(
+  //           fontWeight: FontWeight.w500,
+  //           color: Colors.black54,
+  //           fontSize: 13.0
+  //         ),
+  //         onTap: () {
+  //           showStatefulDialogBuilder(context);
+  //         },
+  //       )
+  //     ],
+  //   );
+  // }
 
+  // 투두 리스트 그려주는 위젯
   Widget _buildTodoListWidget(TodoType todo) {
     return ListTile(
       onTap: () => _donecheckTodo(todo),
-      trailing: IconButton(
+      trailing: IconButton( // 끝쪽(오른쪽)에, 왼쪽에 두고싶으면 leading 파라미터에 주기
         icon: Icon(Icons.delete),
         onPressed: () => _deleteTodo(todo),
       ),
@@ -205,13 +287,28 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
               }
             ),
-            Text(
-              todo.title,
-              style: todo.isDone?
-                  TextStyle(
-                    decoration: TextDecoration.lineThrough,
-                    fontStyle: FontStyle.italic,
-                  ) : null, // 완료 전이면 취소선 안 긋고 아무 짓 안 한다는 표시
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children : [
+                Text(
+                  todo.title,
+                  style: todo.isDone?
+                      TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                        fontStyle: FontStyle.italic,
+                      ) : null, // 완료 전이면 취소선 안 긋고 아무 짓 안 한다는 표시
+                ),
+                Text(
+                  DateFormat('yyyy/MM/dd HH:mm').format(todo.startDate!)
+                    + ' 부터  '
+                    + DateFormat('yyyy/MM/dd HH:mm').format(todo.endDate!)
+                    + ' 까지 '
+                    ,
+                  style: (todo.endDate!.compareTo(DateTime.now()) == 1)?
+                      TextStyle(fontSize: 12, color: Colors.black45)
+                      : TextStyle(fontSize: 12, color: Colors.red[200])
+                ),
+              ]
             )
           ]
         )
